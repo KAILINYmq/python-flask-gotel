628845fa26e77ae668b034150321b018ecf7bf16
1481293524@qq.com <1481293524@qq.com>
2021-02-18 17:25:09 +0800

    ddd

--- agile/api/views.py
+++ agile/api/views.py
@@ -3,8 +3,8 @@ from flask import Blueprint, jsonify
 from flask_restplus import Api
 from agile.api.resources import *
 from agile.api.resources.global_ import Ping
-from agile.api.resources.learning import AddMyLearn,GetAllLearn,SortSearch,UpdataLearn
-from agile.api.resources.idea import AddMyIdea,GetAllIdea,SortSearchIdea,UpdataIdea
+from agile.api.resources.learning import AddMyLearn,GetAllLearn,SortSearch,UpdataLearn,Praises
+from agile.api.resources.idea import AddMyIdea,GetAllIdea,SortSearchIdea,UpdataIdea,PraisesIdea
 from agile.api.resources.tag import ShowFeedback
 
 blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
@@ -45,12 +45,13 @@ api.add_resource(AddMyLearn, "/addLearn")
 api.add_resource(GetAllLearn, "/getAll")
 api.add_resource(SortSearch, "/search")
 api.add_resource(UpdataLearn, "/updata")
-
+api.add_resource(Praises,"/praise")
 # idea
 api.add_resource(AddMyIdea, "/addIdea")
 api.add_resource(GetAllIdea, "/getAllIdea")
 api.add_resource(SortSearchIdea, "/searchIdea")
 api.add_resource(UpdataIdea, "/updataIdea")
+api.add_resource(PraisesIdea,"/praiseIdea")
 
 # Tag
 api.add_resource(ActivityName, "/aName/getAll")
