698127811a12275af1eed18baaa2851d5fe1ff90
ymq <2534992864@qq.com>
2021-02-22 20:37:49 +0800

    active API

--- agile/app.py
+++ agile/app.py
@@ -60,7 +60,8 @@ def configure_extensions(app, cli):
     ma.init_app(app)
     jwt.init_app(app)
     cache.init_app(app)
-    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    # CORS(app, resources={r"/api/*": {"origins": "*"}})
+    CORS(app, supports_credentials=True)
     # if cli is True:
     migrate.init_app(app, db)
 
