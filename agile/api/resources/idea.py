9bf57e8870b0dddce9cce13a94dff189371a9b83
1481293524@qq.com <1481293524@qq.com>
2021-02-23 09:00:33 +0800

    :修改日期只返回年月日

--- agile/api/resources/idea.py
+++ agile/api/resources/idea.py
@@ -213,7 +213,7 @@ class SortSearchIdea(Resource):
                 dict["id"] = val.id
                 dict["name"] = val.name
                 dict["description"] = val.description
-                dict["time"] = value.update_time.strftime("%Y/%m/%d")
+                dict["time"] = val.update_time.strftime("%Y/%m/%d")
                 if val.image is not None and len(val.image) > 0:
                     dict["image"] = json.loads(val.image)
                 else:
