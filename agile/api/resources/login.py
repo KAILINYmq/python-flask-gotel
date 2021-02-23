a49ced1297c0917ad160c4d6748b490d142d9db8
huyunfei <89713373@qq.com>
2021-02-23 18:01:06 +0800

    fix:fix get date interface

--- agile/api/resources/login.py
+++ agile/api/resources/login.py
@@ -23,23 +23,27 @@ class GetHighLightDate(Resource):
     def get(self):
         try:
             # 1. 获取数据
-            date = request.args.get("date").split("-")
-            # 存取endTime对12进行特殊处理
-            if date[1] == "12":
-                end = datetime.datetime(year=int(date[0]) + 1, month=1, day=1)
-            else:
-                end = datetime.datetime(year=int(date[0]), month=int(date[1]) + 1, day=1)
+            setDay = "参数有误。 "
+            date = request.args.get("date")
+            if date is not None:
+                date = date.split("-")
+                print(date)
+                # 存取endTime对12进行特殊处理
+                if date[1] == "12":
+                    end = datetime.datetime(year=int(date[0]) + 1, month=1, day=1)
+                else:
+                    end = datetime.datetime(year=int(date[0]), month=int(date[1]) + 1, day=1)
 
-            start = datetime.datetime(year=int(date[0]), month=int(date[1]), day=1)
-            # 查询在本月有多少条数据
-            data = db.session.query(Activities).filter(Activities.create_time >= start,
-                                                       Activities.create_time < end).all()
-            print(data)
-            setDay = set()
+                start = datetime.datetime(year=int(date[0]), month=int(date[1]), day=1)
+                # 查询在本月有多少条数据
+                data = db.session.query(Activities).filter(Activities.create_time >= start,
+                                                           Activities.create_time < end).all()
+                print(data)
+                setDay = set()
 
-            for i in data:
-                # 从日期中分割出月份并转成int存到set中去重
-                setDay.add(int(str(i.create_time).split("-")[2][0:2]))
+                for i in data:
+                    # 从日期中分割出月份并转成int存到set中去重
+                    setDay.add(int(str(i.create_time).split("-")[2][0:2]))
 
             return ApiResponse(setDay, ResposeStatus.Success)
         except RuntimeError:
